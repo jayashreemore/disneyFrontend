588144a341a1943a3d1af170417d61f2398304a5
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-04-29 13:06:14 -0400

    added pages to the project- Register, Login, review

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function About() {
+    return (
+        <div>
+            <h2 >This project is all about Disney Movies Characters Prince N Princess details</h2>
+            <p> lorem </p>
+        </div>
+    );
+}
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function Home() {
+    return (
+        <div>
+            <h2 >Welcome to my Disney Movie Prince N Princess Project!</h2>
+            <p> lorem </p>
+        </div>
+    );
+}
+export default Home
